ea985fe7b079907bbac5dc7ce5a242a7619cf0e6
PatrikDreve <patrik.drevestedt@me.com>
2015-01-09 14:17:45 +0100

    Source fil

---
+++ Test-Learning Code/Test-Learning Code/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test-Learning Code
+//
+//  Created by Patrik Drevestedt on 2015-01-09.
+//  Copyright (c) 2015 WorkhorsePS. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
