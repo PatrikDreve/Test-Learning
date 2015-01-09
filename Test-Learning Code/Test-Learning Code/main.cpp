0cb7bba477a2a6eb698dd10196f8fec24e0dd96b
PatrikDreve <patrik.drevestedt@me.com>
2015-01-09 16:00:39 +0100

    Ändringar i main.cpp: Structar.

--- Test-Learning Code/Test-Learning Code/main.cpp
+++ Test-Learning Code/Test-Learning Code/main.cpp
@@ -8,9 +8,28 @@
 
 #include <iostream>
 
+//Testande av Structar
+
+struct CAR {
+    char brand[10];
+    int doors;
+    char reg[7];
+};
+
 int main(void) {
     
-    printf("Testande och lärande av ny kod.");
+    int array[20], i, n;
+    char buf[10];
+    struct CAR bil;
+    
+    i = 0;
+    bil.doors = 4;
+    n = 42;
+    
+    strcpy(buf, "Kalle");
+    strcpy(bil.brand, "Volvo");
+    strcpy(bil.reg, "ABC123");
+    
     
     return 0;
 }
